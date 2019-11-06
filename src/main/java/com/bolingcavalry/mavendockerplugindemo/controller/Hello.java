794ef17de750220ef75135362d731866eca36119
xienanjiang <672088678@qq.com>
2019-11-06 17:01:52 +0800

    1

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        return "第一次调试. Hello jenkins, " + new Date();
+        return "第二次调试. Hello jenkins, " + new Date();
     }
 }
