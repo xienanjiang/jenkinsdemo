a93351b7d77c5cc4e92eb62b6f6db1839b08ffa8
xienanjiang <672088678@qq.com>
2019-11-06 15:32:31 +0800

    1

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        return "xnj123124. Hello jenkins, " + new Date();
+        return "xnj1231241123124. Hello jenkins, " + new Date();
     }
 }
